cb73c1228abd70cc35265511babb2d8f68fe189b
babusurendra <babusurendra.p@gmail.com>
2017-03-21 20:18:09 +0530

    added new xpath

--- MarketsPagePO.rb
+++ MarketsPagePO.rb
@@ -3,10 +3,7 @@ module MarketsPagePO
     Index_xpath = "//XCUIElementTypeTable[@label='data grid']/XCUIElementTypeOther//XCUIElementTypeStaticText"
     ColumnHdr_xpath ="//XCUIElementTypeTable[@label='column header grid']//XCUIElementTypeCell[@label='column header 0']//XCUIElementTypeStaticText"
     IpadMrktsColsData_xpath = "//XCUIElementTypeTable[@label='data grid']/XCUIElementTypeCell['@varData']/XCUIElementTypeOther[@label='DataValues']"
-    #IPadColHdr_xpath = "//XCUIElementTypeTable[@label='column header grid']/UIATableCell[@label='column header 0']/UIAStaticText[@label='#{colName}']"
-    
-    #IPadColHdr_xpath = "//XCUIElementTypeTable[@label='column header grid']/UIATableCell[@label='column header 0']/UIAStaticText[@label='#{colName}']"
-    
+
     def MarketsPagePO.ReplaceString(xpath, replacingWord, originalWord = "@varData")
            return xpath.gsub originalWord, replacingWord
     end

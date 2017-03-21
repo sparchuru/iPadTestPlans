c687903da2c1e6b32e32499c2a8b746d5c8fd9d1
babusurendra <babusurendra.p@gmail.com>
2017-03-21 20:15:39 +0530

    added new xpath

--- MarketsPagePO.rb
+++ MarketsPagePO.rb
@@ -2,6 +2,8 @@ module MarketsPagePO
     Market_xpath = "//XCUIElementTypeTable[@label='FDSMarketViewController Table']//*[@label='@varData']"
     Index_xpath = "//XCUIElementTypeTable[@label='data grid']/XCUIElementTypeOther//XCUIElementTypeStaticText"
     ColumnHdr_xpath ="//XCUIElementTypeTable[@label='column header grid']//XCUIElementTypeCell[@label='column header 0']//XCUIElementTypeStaticText"
+    IpadMrktsColsData_xpath = "//XCUIElementTypeTable[@label='data grid']/XCUIElementTypeCell['@varData']/XCUIElementTypeOther[@label='DataValues']"
+    #IPadColHdr_xpath = "//XCUIElementTypeTable[@label='column header grid']/UIATableCell[@label='column header 0']/UIAStaticText[@label='#{colName}']"
     
     def MarketsPagePO.ReplaceString(xpath, replacingWord, originalWord = "@varData")
            return xpath.gsub originalWord, replacingWord

9922bde9d9241e879561468d503f30d546de36b1
babusurendra <babusurendra.p@gmail.com>
2017-03-22 20:43:31 +0530

    pushing missed changes in previous checkin

--- CommonObjects.rb
+++ CommonObjects.rb
@@ -2,7 +2,7 @@ module CommonObjects
     
     StaticText_xpath = "//XCUIElementTypeStaticText[@label='@varData']"
     ButtonType_xpath = "//XCUIElementTypeButton[@label='@varData']"
-    
+    ContainsLabel_xpath = "//XCUIElementTypeStaticText[contains(@label,'@varData')]"
     def CommonObjects.ReplaceString(xpath, replacingWord, originalWord = "@varData")
            return xpath.gsub originalWord, replacingWord
     end

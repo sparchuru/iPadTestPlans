935b15f07bf305bc98fc72869d86e4a3dce8c165
babusurendra <babusurendra.p@gmail.com>
2017-03-22 20:40:06 +0530

    added new xpath

--- CommonObjects.rb
+++ CommonObjects.rb
@@ -1,7 +1,8 @@
 module CommonObjects
     
     StaticText_xpath = "//XCUIElementTypeStaticText[@label='@varData']"
-    ButtonType_xpath = "//XCUIElementTypeStaticButton[@label='@varData']"
+    ButtonType_xpath = "//XCUIElementTypeButton[@label='@varData']"
+    
     def CommonObjects.ReplaceString(xpath, replacingWord, originalWord = "@varData")
            return xpath.gsub originalWord, replacingWord
     end

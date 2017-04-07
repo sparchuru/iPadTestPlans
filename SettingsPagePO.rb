cc87a45e662ec084cbe0e3be44a66e5c8f5e9703
kommu swamy <swamy.kommu@factset.com>
2017-04-07 18:21:36 +0530

    Update SettingsPagePO.rb
    removed old Xpaths

--- SettingsPagePO.rb
+++ SettingsPagePO.rb
@@ -3,19 +3,11 @@ module SettingsPagePO
     CommonXpath  = "//XCUIElementTypeStaticText[@label='@varData']" # for all static text type in  general page.
     ButtontypeXpath = "//XCUIElementTypeButton[@label='@varData']"  # buttons in General page 
     NavigationtileXpath = "//XCUIElementTypeImage[@name='TileNav_Q']"   #  arrow mark in home page setup window 
-    
-    
-    
-    
-    
-    
-    
-    # Inhouse page objects
-    
-     LimaOverRideFieldXpath = '//XCUIElementTypeTextField[@label="override name"]'
-     #AlertOkXpath = ''
-     UseStagingSwitchXpath = "//XCUIElementTypeSwitch[@label='staging switch']"
-     InhousePageElementsXpath = "//XCUIElementTypeStaticText[@label='@varData'] | //XCUIElementTypeButton[@label='@varData']"
+   # Inhouse page objects
+   LimaOverRideFieldXpath = '//XCUIElementTypeTextField[@label="override name"]'
+  #AlertOkXpath = ''
+   UseStagingSwitchXpath = "//XCUIElementTypeSwitch[@label='staging switch']"
+   InhousePageElementsXpath = "//XCUIElementTypeStaticText[@label='@varData'] | //XCUIElementTypeButton[@label='@varData']"
     def SettingsPagePO.ReplaceString(xpath, replacingWord, originalWord = "@varData")
            return xpath.gsub originalWord, replacingWord
     end
